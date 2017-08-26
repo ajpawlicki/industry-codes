0a455c72a3213fd36760353f82d78461a7c35a7b
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-08-26 15:16:18 -0700

    Configure environment variables

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
@@ -9,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../client/dist'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log(`Listening on port ${port}`);
