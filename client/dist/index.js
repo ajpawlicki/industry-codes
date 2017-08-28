0aac20a706500b354d2ec31e121915812ea75572
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-08-28 02:25:30 -0700

    Create basic user interface to lookup codes

---
+++ client/dist/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+window.onload = () => {
+  let typeInput = document.getElementById('type-input');
+  let codeInput = document.querySelector('#code-input');
+  let codeList = document.getElementById('code-list');
+  let form = document.querySelector('form');
+  let trimmedInputValue;
+  
+  codeInput.setAttribute('autocomplete', 'off');
+  
+  const addToList = (arr, list) => {
+    arr.forEach((item) => {
+      let listItem = document.createElement('li');
+
+      listItem.innerHTML = `
+        <div>ISO Description: ${item.ISO_Description}</div>
+        <div>ISO CGL: ${item.ISO_CGL}</div>
+        <div>SIC: ${item.SIC}</div>
+        <div>NAICS: ${item.NAICS}</div>
+        <div>General Description: ${item.General_Description}</div>
+        <div>NCCI: ${item.NCCI}</div>
+        <div>CA WC: ${item.CA_WC}</div>
+      `;
+      list.appendChild(listItem);
+    });
+  };
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    trimmedInputValue = codeInput.value.trim();
+
+    codeList.innerHTML = '';
+    codeInput.value = ''
+
+    if (trimmedInputValue.length > 0) {
+      $.get(`/api/codes/type/${typeInput.value}/code/${trimmedInputValue}`, (codes) => {
+        if (codes.length > 0) {
+          addToList(codes, codeList);
+        } else {
+          codeList.innerHTML = 'There is no data for that code.';
+        }
+      });
+    }
+
+  });
+};
