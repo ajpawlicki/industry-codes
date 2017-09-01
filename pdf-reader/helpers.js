28b86620b19925bb89aad91cf6224aa015a315d8
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-08-31 17:30:21 -0700

    Abstract parser helper functions to separate file

---
+++ pdf-reader/helpers.js
@@ -0,0 +1,95 @@
+module.exports.generateMatrix = function(rows) {
+  const keys = Object.keys(rows);
+  
+  if (keys.length > 0) {
+    const matrix = [];
+    keys
+      .sort((y1, y2) => parseFloat(y1) - parseFloat(y2))
+      .forEach((key) => matrix.push(rows[key]));
+    
+    return matrix;
+  } else {
+    return null;
+  }
+};
+
+module.exports.handleTextOverFlow = function(matrix) {
+  let row, overflow, prev, str;
+
+  const trailingSymbols = {
+    ' ': true,
+    '-': true
+  };
+  
+  for (let i = 0; i < matrix.length; i++) {
+    row = matrix[i];
+    if (row.length <= 2) {
+      
+      while (row.length > 0) {
+        overflow = row.shift();
+        prev = matrix[i-1];
+        
+        for (let j = 0; j < prev.length; j++) {
+          str = prev[j];
+          if (trailingSymbols[str[str.length - 1]] === true) {
+            prev[j] += overflow;
+            break;
+          }
+        }
+      }
+
+      matrix.splice(i, 1);
+      i--;
+    }
+  }
+};
+
+module.exports.unshiftMissingCells = function(matrix) {
+  for (let row of matrix) {
+    while (row.length < 13) {
+      row.unshift(null);
+    }
+  }
+};
+
+module.exports.populateMissingText = function(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  // iterate thru matrix
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      // if cell in row above and cell in row below are null
+      if (isSafeToVisit(matrix, i-1, j) && isSafeToVisit(matrix, i+1, j) && matrix[i][j] !== null) {
+        // call DFS
+        DFS(matrix, i, j, matrix[i][j]);
+      }
+    }
+  }
+};
+
+function isSafeToVisit(matrix, row, col) {
+  const isValidRow = (row >= 0) && (row < matrix.length);
+  const isValidCol = (col >= 0) && (col < matrix[0].length);
+  return isValidRow && isValidCol && matrix[row][col] === null;
+};
+
+function DFS(matrix, row, col, text) {
+  matrix[row][col] = text;
+  
+  if (isSafeToVisit(matrix, row-1, col)) DFS(matrix, row-1, col, text);
+  if (isSafeToVisit(matrix, row+1, col)) DFS(matrix, row+1, col, text);
+};
+
+module.exports.postToDB = function(matrix) {
+  const headers = matrix.shift().map(str => str.replace(' ', '_'));
+  let codeData;
+
+  matrix.forEach((row) => {
+    codeData = row.reduce((acc, curr, index) => {
+      acc[headers[index]] = curr;
+      return acc;
+    }, {});
+        
+    postCode(codeData);
+  });
+};
--- pdf-reader/parser.js
+++ pdf-reader/parser.js
@@ -1,7 +1,8 @@
 const pdfreader = require('pdfreader');
 const filepath = '/assets/13.pdf'
 
-const postCode = require('../database/helpers.js').postCode;
+const generateMatrix = require('./helpers.js').generateMatrix;
+const postToDB = require('./helpers.js').postToDB;
 
 let rows = {}; // indexed by y-position
 
@@ -18,8 +19,6 @@ module.exports = function() {
 
       if (matrix) {
         matrix.pop(); // remove last row b/c irrelevant to data
-        handleTextOverFlow(matrix);
-        unshiftMissingCells(matrix);
         postToDB(matrix);
         // matrix.forEach((row) => console.log(row.join(' | ')));
       }
@@ -33,101 +32,5 @@ module.exports = function() {
       // accumulate text items into rows object, per line
       (rows[item.y] = rows[item.y] || []).push(item.text);
     }
-  });
-}
-
-function generateMatrix(rows) {
-  const keys = Object.keys(rows);
-  
-  if (keys.length > 0) {
-    const matrix = [];
-    keys
-      .sort((y1, y2) => parseFloat(y1) - parseFloat(y2))
-      .forEach((key) => matrix.push(rows[key]));
-    
-    return matrix;
-  } else {
-    return null;
-  }
-};
-
-function handleTextOverFlow(matrix) {
-  let row, overflow, prev, str;
-
-  const trailingSymbols = {
-    ' ': true,
-    '-': true
-  };
-  
-  for (let i = 0; i < matrix.length; i++) {
-    row = matrix[i];
-    if (row.length <= 2) {
-      
-      while (row.length > 0) {
-        overflow = row.shift();
-        prev = matrix[i-1];
-        
-        for (let j = 0; j < prev.length; j++) {
-          str = prev[j];
-          if (trailingSymbols[str[str.length - 1]] === true) {
-            prev[j] += overflow;
-            break;
-          }
-        }
-      }
-
-      matrix.splice(i, 1);
-      i--;
-    }
-  }
-};
-
-function unshiftMissingCells(matrix) {
-  for (let row of matrix) {
-    while (row.length < 13) {
-      row.unshift(null);
-    }
-  }
-};
-
-function populateMissingText(matrix) {
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  // iterate thru matrix
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      // if cell in row above and cell in row below are null
-      if (isSafeToVisit(matrix, i-1, j) && isSafeToVisit(matrix, i+1, j) && matrix[i][j] !== null) {
-        // call DFS
-        DFS(matrix, i, j, matrix[i][j]);
-      }
-    }
-  }
-};
-
-function isSafeToVisit(matrix, row, col) {
-  const isValidRow = (row >= 0) && (row < matrix.length);
-  const isValidCol = (col >= 0) && (col < matrix[0].length);
-  return isValidRow && isValidCol && matrix[row][col] === null;
-};
-
-function DFS(matrix, row, col, text) {
-  matrix[row][col] = text;
-  
-  if (isSafeToVisit(matrix, row-1, col)) DFS(matrix, row-1, col, text);
-  if (isSafeToVisit(matrix, row+1, col)) DFS(matrix, row+1, col, text);
-};
-
-function postToDB(matrix) {
-  const headers = matrix.shift().map(str => str.replace(' ', '_'));
-  let codeData;
-
-  matrix.forEach((row) => {
-    codeData = row.reduce((acc, curr, index) => {
-      acc[headers[index]] = curr;
-      return acc;
-    }, {});
-        
-    postCode(codeData);
   });
 };
